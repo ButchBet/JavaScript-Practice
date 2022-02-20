e6d05b4ed638348718201d01b7defabe85544470
ButchBet <butchbet01@gmail.com>
2022-02-20 13:01:42 -0500

    The proccess goes fine, by now the style of the header and the main are great, the Task components has been created successfuly and the conection to add dinamically information has been built.

--- components/task.js
+++ components/task.js
@@ -3,16 +3,52 @@ class Task extends HTMLElement {
         super();
 
         this.attachShadow({mode: "open"});
-
-        console.log(this.id, this.desciption, this.value);
     }
 
     static get styles() {
-        return ``;
+        return `
+            :host {
+                display: flex;
+                flex-flow: column wrap;
+                justify-content: space-between;
+            }
+
+            #image {
+                display: flex;
+                flex-flow: row wrap;
+                justify-content: space-between;
+                align-items: center;
+            }
+
+            #image img  {
+                width: 100px;
+            }
+
+            #info {
+                text-align: center;
+            }
+        `;
     }
 
     connectedCallback() {
-        this.render();
+        const option = {
+            method: "GET"
+        }
+
+        fetch("../data/tasks.json", option)
+        .then((response) => response.json())
+        .then((file) => {
+            // Get the data using the id
+            const data = file[this.id];
+
+            // Initializate the image url, title and description
+            this.image = data.img;
+            this.title = data.name;
+            this.description = data.description;
+
+            this.render();
+        })
+        .catch((err) => console.log(err));
     }
 
     disconnectedCallback() {}
@@ -21,8 +57,18 @@ class Task extends HTMLElement {
         this.shadowRoot.innerHTML = 
         `
             <style>${Task.styles}</style>
+            
+            <section id="image">
+                <img src="${this.image}"
+                <h1>${this.title}<h1>
+            </section>
+
+            <section id="info">
+                <p>${this.description}</p>
+            </section>
         `;
     }
 }
 
 customElements.define("app-task", Task);
+
